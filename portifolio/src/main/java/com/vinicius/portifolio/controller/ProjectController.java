89b99fce7153af05f925cc4ab3fc98733caa2614
ViniDias1 <viniciusdvalenca@gmail.com>
2025-02-24 09:52:35 -0300

    Add Repo

--- portifolio/src/main/java/com/vinicius/portifolio/controller/ProjectController.java
+++ portifolio/src/main/java/com/vinicius/portifolio/controller/ProjectController.java
@@ -1,17 +1,17 @@
 package com.vinicius.portifolio.controller;
 
 import com.vinicius.portifolio.dto.ProjectDTO;
+import com.vinicius.portifolio.repository.ProjectsRepository;
 import com.vinicius.portifolio.service.ProjectService;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/projects")
 public class ProjectController {
-
+    
     private final ProjectService projectService;
 
     public ProjectController(ProjectService projectService) {
@@ -19,13 +19,26 @@ public class ProjectController {
     }
 
     @GetMapping
-    public List<ProjectDTO> getProjects() {
+    public ProjectsRepository getProjects() {
         return projectService.getAllProjects();
     }
 
-    @PostMapping("/post")
+    @PostMapping("/addProject")
     public ResponseEntity<ProjectDTO> createProject(@RequestBody ProjectDTO project) {
         ProjectDTO newProject = projectService.addProject(project);
         return ResponseEntity.status(HttpStatus.CREATED).body(newProject);
+    }
+
+    
+
+    
+    @GetMapping("/testeGET")
+    public String testeGET() {
+        return "teste";
+    }
+
+    @PostMapping("/testePOST")
+    public String testePOST(@RequestBody String teste){
+        return teste;
     }
 }
